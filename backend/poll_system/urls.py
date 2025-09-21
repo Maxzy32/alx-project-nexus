fe0bd3a88acfc81bdc1452b59e88356a7da44446
CaptaiRich <ahiakpormaxwell32@gmail.com>
2025-09-21 11:55:26 +0000

    feat: integrate Swagger documentation

--- backend/poll_system/urls.py
+++ backend/poll_system/urls.py
@@ -37,19 +37,19 @@ from drf_yasg import openapi
 from django.conf.urls.static import static
 
 
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="ALX Project Nexus API",
-#         default_version="v1",
-#         description="API documentation for Users, Polls, Candidates, Votes, and Authentication",
-#         terms_of_service="https://www.google.com/policies/terms/",
-#         contact=openapi.Contact(email="support@alx-nexus.com"),
-#         license=openapi.License(name="MIT License"),
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-#     urlconf="poll_system.urls",  # ✅ point to the correct urls.py
-# )
+schema_view = get_schema_view(
+    openapi.Info(
+        title="ALX Project Nexus API",
+        default_version="v1",
+        description="API documentation for Users, Polls, Candidates, Votes, and Authentication",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="support@alx-nexus.com"),
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+    urlconf="poll_system.urls",  # ✅ point to the correct urls.py
+)
 
 
 router = DefaultRouter()
@@ -67,10 +67,10 @@ urlpatterns = [
      # Include votes app URLs
     path("api/votes/", include("votes.urls")),
 
-    # # Swagger docs
-    # re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
-    # path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
-    # path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
+    # Swagger docs
+    re_path(r"^swagger(?P<format>\.json|\.yaml)$", schema_view.without_ui(cache_timeout=0), name="schema-json"),
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 
     ]
 
